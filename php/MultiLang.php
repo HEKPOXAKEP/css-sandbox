7fe02fefc161451248d1ab80a38461d089859f21
Tosh Judeinov <hellish.engineer@gmail.com>
2024-02-12 15:56:08 +0300

    Added WZZ function for formating messages with args

--- php/MultiLang.php
+++ php/MultiLang.php
@@ -5,9 +5,38 @@
   ======================
 */
 
+require_once('msgs-def.php');
 require_once('msgs-en.php');
 
-function WZ(string $mi): string {
+function WZ(int $mi): string {
   return(MSGS[$mi]);
+}
+
+/*function WZZ(string $mi, mixed ...$args): string {
+  //print_r($args); return 'qwerty';
+  $s=MSGS[$mi];
+  return(sprintf($s,$args));
+}*/
+
+/*
+  Работает, как vsprintf, но вместо индекса аргументов $args принимает ключи.
+
+  Директива '%s' без имени аргумента тоже работает нормально. Поддерживается всё,
+  что может сделать vsprintf().
+
+  Пример: vskprintf('y = %y$d, x = %x$1.1f', array('x' => 1, 'y' => 2))
+  Результат: 'y = 2, x = 1.0'
+
+  Автор оригинала: Josef Kufner <jkufner(at)gmail.com>
+*/
+function WZZ(int $mi, array $args): string {
+  $frms=MSGS[$mi];
+  $map=array_flip(array_keys($args));
+
+  $new_str=preg_replace_callback('/(^|[^%])%([a-zA-Z0-9_-]+)\$/',
+    function($m) use ($map) { return $m[1].'%'.($map[$m[2]] + 1).'$'; },
+    $frms);
+
+  return vsprintf($new_str,$args);
 }
 ?>
