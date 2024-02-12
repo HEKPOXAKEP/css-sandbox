6980300375b87739a5ed06866e9d9594fd0b103f
Tosh Judeinov <hellish.engineer@gmail.com>
2024-02-12 15:54:09 +0300

    Message IDs definition constants

---
+++ php/msgs-def.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  ===================
+  Индексы сообщений
+  ===================
+*/
+const
+  I_1=-1, // for testing WZZ()
+
+  // --- Information messages
+  I_Ok=1001,
+  I_FileDeleted=1001,
+  I_FileSaved=1002,
+  I_FileLoaded=1003,
+
+  // Error msg identifiers
+  E_FileNotExists=3001,
+  E_ChkDataDirErr=3002;
+?>
--- php/msgs-en.php
+++ php/msgs-en.php
@@ -4,6 +4,16 @@
 */
 const
   MSGS=[
-    'E_ChkDataDirErr'=>'CheckDataDir() returned an error.',
+    I_1=>'This is a %num$d test of %that$s.',  ///dbg msg - for testing WZZ() etc.
+
+    // --- Information messages
+    I_Ok=>'Ok.',
+    I_FileDeleted=>'File %fn$s deleted.',
+    I_FileSaved=>'File %fn$s saved.',
+    I_FileLoaded=>'File %fn$s loaded.',
+
+    // --- Error messages
+    E_FileNotExists=>'File %fn$s does not exists.',
+    E_ChkDataDirErr=>'CheckDataDir() returned an error.',
   ];
 ?>
